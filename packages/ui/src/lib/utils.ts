eaf7b768e92cf8a8f4315ff4d64a3ca57e15cb6e
sangrimlee <contact@sangrim.dev>
2022-12-21 21:30:26 +0900

    feat: splitProps 유틸 추가
    - sprinkles의 key인지 다른 Props인지 판별하는 유틸함수 추가
    - Reference: https://github.com/Shopify/foundational-design-system-proto

--- packages/ui/src/lib/utils.ts
+++ packages/ui/src/lib/utils.ts
@@ -1,3 +1,23 @@
-export const mapVarName = (_value: string | null, path: string[]) => {
-  return path.join('-').replace(/\./g, '_').replace(/\//g, '__');
+import { Sprinkles, sprinkles } from '../css';
+
+export const isSprinkles = (key: string): key is keyof Sprinkles => {
+  return sprinkles.properties.has(key as keyof Sprinkles);
+};
+
+export const splitProps = <T extends { [key: string]: any }>(props: T) => {
+  const nativeProps: { [key: string]: unknown } = {};
+  const sprinklesProps: { [key: string]: unknown } = {};
+
+  for (const key in props) {
+    if (isSprinkles(key)) {
+      sprinklesProps[key] = props[key as keyof typeof props];
+    } else {
+      nativeProps[key] = props[key as keyof typeof props];
+    }
+  }
+
+  return {
+    nativeProps,
+    sprinklesProps,
+  };
 };
