b1684c01305cf54d53cbc796405fcddc01956813
sangrimlee <contact@sangrim.dev>
2022-12-29 20:03:12 +0900

    feat: argTypes를 위한 유틸 함수 수정 및 추가
    - children argType을 위한 유틸 함수 추가
    - 보기 좋게 수정

--- apps/docs/src/utils/argTypes.ts
+++ apps/docs/src/utils/argTypes.ts
@@ -1,28 +1,46 @@
-export const selectArgTypes = (description: string, options: string[], defaultValue?: string) => {
+import type { InputType } from '@storybook/csf';
+
+export const selectArgType = <T extends number | string>(
+  description: string,
+  options: T[],
+  defaultValue?: T,
+): InputType => {
   return {
     control: 'select',
-    description: description,
+    description,
     options: options,
     defaultValue,
     table: {
       type: {
-        summary: options.join(', '),
+        summary: options.map((option) => option.toString()).join(' | '),
       },
       defaultValue: { summary: defaultValue },
     },
   };
 };
 
-export const booleanArgTypes = (description: string, defaultValue = false) => {
+export const booleanArgType = (description: string, defaultValue = false): InputType => {
   return {
     control: 'boolean',
-    description: description,
+    description,
     defaultValue,
     table: {
       type: {
-        summary: ['true', 'false'].join(', '),
+        summary: 'true | false',
       },
       defaultValue: { summary: Boolean(defaultValue) },
+    },
+  };
+};
+
+export const childrenArgType = (description?: string): InputType => {
+  return {
+    control: 'text',
+    description: description ?? '자식 컴포넌트 또는 컨텐츠',
+    table: {
+      type: {
+        summary: 'ReactNode',
+      },
     },
   };
 };
