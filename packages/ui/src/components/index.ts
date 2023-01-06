d0da0c653daeb5fd0ff8f188a7f6199d672109b6
sangrimlee <contact@sangrim.dev>
2023-01-06 15:42:56 +0900

    feat: Dialog 컴포넌트 export 추가
    - Dialog 컴포넌트 export 추가

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -1,5 +1,6 @@
 export * from './Alert';
 export * from './Button';
+export * from './Dialog';
+export * from './Logo';
 export * from './SocialButton';
 export * from './Text';
-export * from './Logo';
