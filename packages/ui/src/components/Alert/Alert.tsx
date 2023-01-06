dccd8947b559a557aa2df015900c734ab9908132
sangrimlee <contact@sangrim.dev>
2023-01-06 15:43:48 +0900

    fix: Alert 컴포넌트에 대한 any로 타입 지정되는 것 수정
    - CSSProp을 확장하지 않고 값으로 추가
    - 제대로 되지 않은 이름들 수정

--- packages/ui/src/components/Alert/Alert.tsx
+++ packages/ui/src/components/Alert/Alert.tsx
@@ -1,58 +1,63 @@
 import * as AlertDialogPrimitive from '@radix-ui/react-alert-dialog';
 import { Flex } from 'layouts';
 import React from 'react';
-import { CSSProp } from 'stitches.config';
+import { CSS } from 'stitches.config';
 
 import { Button } from '../Button';
 import { Text } from '../Text';
 import { StyledAlertContent, StyledAlertContentInner, StyledAlertOverlay } from './Alert.styled';
 
-type AlertDialogPrimitivePrimitiveProps = React.ComponentProps<typeof AlertDialogPrimitive.Content>;
-type AlertContentProps = AlertDialogPrimitivePrimitiveProps &
-  CSSProp & {
-    title: React.ReactNode;
-    description: React.ReactNode;
-    actionLabel?: React.ReactNode;
-    cancelLabel?: React.ReactNode;
-    onAction?: React.MouseEventHandler<HTMLButtonElement>;
-  };
+type AlertDialogPrimitiveProps = React.ComponentProps<typeof AlertDialogPrimitive.Content>;
+type AlertContentProps = AlertDialogPrimitiveProps & {
+  title: React.ReactNode;
+  description: React.ReactNode;
+  actionLabel?: React.ReactNode;
+  cancelLabel?: React.ReactNode;
+  onAction?: React.MouseEventHandler<HTMLButtonElement>;
+  css?: CSS;
+};
 
 const AlertContent = React.forwardRef<
   React.ElementRef<typeof StyledAlertContent>,
   AlertContentProps
->(({ title, description, actionLabel = '확인', cancelLabel = '취소', onAction }, forwardedRef) => {
-  return (
-    <AlertDialogPrimitive.Portal>
-      <StyledAlertOverlay />
-      <StyledAlertContent ref={forwardedRef}>
-        <StyledAlertContentInner>
-          <AlertDialogPrimitive.Title asChild>
-            <Text as="h2" weight="bold" size="xl" css={{ marginBottom: '$2' }}>
-              {title}
-            </Text>
-          </AlertDialogPrimitive.Title>
-          <AlertDialogPrimitive.Description asChild>
-            <Text as="p" variant="variant" css={{ marginBottom: '$4' }}>
-              {description}
-            </Text>
-          </AlertDialogPrimitive.Description>
-          <Flex spacingX={4}>
-            <AlertDialogPrimitive.Action asChild onClick={onAction}>
-              <Button variant="primary" size="lg" fullWidth>
-                {actionLabel}
-              </Button>
-            </AlertDialogPrimitive.Action>
-            <AlertDialogPrimitive.Cancel asChild>
-              <Button size="lg" fullWidth>
-                {cancelLabel}
-              </Button>
-            </AlertDialogPrimitive.Cancel>
-          </Flex>
-        </StyledAlertContentInner>
-      </StyledAlertContent>
-    </AlertDialogPrimitive.Portal>
-  );
-});
+>(
+  (
+    { title, description, actionLabel = '확인', cancelLabel = '취소', css, onAction, ...props },
+    forwardedRef,
+  ) => {
+    return (
+      <AlertDialogPrimitive.Portal>
+        <StyledAlertOverlay />
+        <StyledAlertContent ref={forwardedRef} {...props}>
+          <StyledAlertContentInner css={css}>
+            <AlertDialogPrimitive.Title asChild>
+              <Text as="h2" weight="bold" size="xl" css={{ marginBottom: '$2' }}>
+                {title}
+              </Text>
+            </AlertDialogPrimitive.Title>
+            <AlertDialogPrimitive.Description asChild>
+              <Text as="p" variant="variant" css={{ marginBottom: '$4' }}>
+                {description}
+              </Text>
+            </AlertDialogPrimitive.Description>
+            <Flex spacingX={4}>
+              <AlertDialogPrimitive.Action asChild onClick={onAction}>
+                <Button variant="primary" size="lg" fullWidth>
+                  {actionLabel}
+                </Button>
+              </AlertDialogPrimitive.Action>
+              <AlertDialogPrimitive.Cancel asChild>
+                <Button size="lg" fullWidth>
+                  {cancelLabel}
+                </Button>
+              </AlertDialogPrimitive.Cancel>
+            </Flex>
+          </StyledAlertContentInner>
+        </StyledAlertContent>
+      </AlertDialogPrimitive.Portal>
+    );
+  },
+);
 
 AlertContent.displayName = 'AlertContent';
 
