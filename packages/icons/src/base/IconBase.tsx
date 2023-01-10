8f840b4447e0d5432c369e19c8eefb197f424a69
sangrimlee <contact@sangrim.dev>
2023-01-10 20:06:54 +0900

    feat: IconBase 컴포넌트 추가
    - svg파일의 기본이 되는 IconBase 컴포넌트 추가
    - width, height가 아닌 size라는 props로 사이즈 변경 기능 추가

---
+++ packages/icons/src/base/IconBase.tsx
@@ -0,0 +1,37 @@
+import React, { forwardRef } from 'react';
+
+export interface IconBaseProps extends React.SVGAttributes<SVGElement> {
+  size?: string | number;
+  title?: string;
+  titleId?: string;
+  children?: React.ReactNode;
+}
+
+const IconBase = forwardRef<SVGSVGElement, IconBaseProps>(
+  ({ size, title, titleId, children, width, height, role, ...svgProps }, forwardedRef) => {
+    const computedWidth = size || width || '1em';
+    const computedHeight = size || height || '1em';
+    const computedTitleId = titleId || 'title';
+
+    return (
+      <svg
+        fill="currentColor"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+        aria-labelledby={computedTitleId}
+        xmlns="http://www.w3.org/2000/svg"
+        {...svgProps}
+        height={computedWidth}
+        width={computedHeight}
+        role={role || 'icon'}
+        ref={forwardedRef}
+      >
+        {title && <title id={computedTitleId}>{title}</title>}
+        {children}
+      </svg>
+    );
+  },
+);
+IconBase.displayName = 'IconBase';
+
+export default IconBase;
