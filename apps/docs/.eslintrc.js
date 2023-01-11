ee598d3e15f3e1c76bad18adc3a410ead93555f6
sangrimlee <contact@sangrim.dev>
2023-01-11 20:59:33 +0900

    chore: mdx 관련 eslint 플러그인 추가
    - eslint-plugin-mdx 의존성 추가
    - mdx 관련 lint를 관련 설정 추가

--- apps/docs/.eslintrc.js
+++ apps/docs/.eslintrc.js
@@ -2,5 +2,5 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ['@triprint', 'plugin:storybook/recommended'],
+  extends: ['@triprint', 'plugin:storybook/recommended', 'plugin:mdx/recommended'],
 };
